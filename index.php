569e530df900dd26e0bc455ba84627e37b48b8f2
Phil Parsons <phil@parsons.uk.com>
2015-01-25 23:55:30 +0100

    Added first draft

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+//to keep it simple using require
+require 'Milight.php';
+
+$milight = new Milight('192.168.1.7');
+
+
+$milight->rgbwAllOn();
+$milight->rgbwAllSetToWhite();
+$milight->rgbwAllBrightnessMax();
+sleep(2);
+$milight->setRgbwActiveGroup(1);
+$milight->rgbwBrightnessPercent(50);
+sleep(2);
+$milight->setRgbwActiveGroup(2);
+$milight->rgbwSetColorHexString('FF1254'); // or #FF1254
+$milight->rgbwBrightnessPercent(90);
+sleep(2);
+$milight->whiteAllOn();
+$milight->whiteAllBrightnessMax();
+sleep(2);
+$milight->whiteGroup1NightMode();
+sleep(2);
+$milight->setWhiteActiveGroup(2);
+$milight->whiteWarmIncrease();
+$milight->whiteWarmIncrease();
+$milight->whiteWarmIncrease();
