ceb14c795987c72bd5618c8430179b336928d9e6
Phil Parsons <phil@parsons.uk.com>
2015-01-26 01:14:43 +0100

    set active group

--- index.php
+++ index.php
@@ -37,7 +37,8 @@
       $milight->rgbwSetColorToRed();
       }
       elseif ($_GET["action"]=="all_25") {
-      $milight->command('rgbwBrightnessPercent(25)');
+      $milight->rgbwSetActiveGroup(0);
+      $milight->rgbwBrightnessPercent(25);
       }
       elseif ($_GET["action"]=="all_100") {
       $milight->rgbwBrightnessPercent(100);
