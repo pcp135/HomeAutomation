97f9149695700d2f8a99b9be316c80c14822f10c
Phil Parsons <phil@parsons.uk.com>
2015-01-26 01:04:49 +0100

    added some more advanced controls

--- index.php
+++ index.php
@@ -5,44 +5,56 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Lights dashboard</title>
-
+    
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
+    
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+      <![endif]-->
   </head>
   <body>
-
-<?php
-
-   //to keep it simple using require
-   require 'Milight.php';
-
-   $milight = new Milight('192.168.1.7');
-   echo $_GET["action"];
-   if ($_GET["action"]=="all_on") {
-   $milight->rgbwAllOn();
-$milight->rgbwAllSetToWhite();
-    $milight->rgbwAllBrightnessMax();
-    echo "clause 1";
-    }
     
-    if ($_GET["action"]=="all_off") {
-    $milight->rgbwAllOff();
-    echo "clause 2";
-    }
+    <?php
 
-    ?>
-
-<h1>Hello, world!</h1>
-
-<a href="index.php?action=all_on" class="btn btn-primary">Lights on</a>
-<a href="index.php?action=all_off" class="btn btn-primary">Lights off</a>
+       //to keep it simple using require
+       require 'Milight.php';
+       
+       $milight = new Milight('192.168.1.7');
+       if ($_GET["action"]=="all_on") {
+       $milight->rgbwAllOn();
+      }
+      elseif ($_GET["action"]=="all_off") {
+      $milight->rgbwAllOff();
+      }
+      elseif ($_GET["action"]=="all_white") {
+    $milight->rgbwAllSetToWhite();
+      }
+      elseif ($_GET["action"]=="all_random") {
+      $milight->rgbwSetColorToRed();
+      }
+      elseif ($_GET["action"]=="all_25") {
+      $milight->rgbwBrightnessPercent(25);
+      }
+      elseif ($_GET["action"]=="all_100") {
+      $milight->rgbwBrightnessPercent(100);
+      }
+      
+      ?>
+    
+    <h1>All lights</h1>
+    
+    <a href="index.php?action=all_on" class="btn btn-primary">On</a>
+    <a href="index.php?action=all_off" class="btn btn-primary">Off</a>
+    <a href="index.php?action=all_white" class="btn btn-primary">White</a>
+    <a href="index.php?action=all_random" class="btn btn-primary">Random</a>
+    <a href="index.php?action=all_25" class="btn btn-primary">25%</a>
+    <a href="index.php?action=all_50" class="btn btn-primary">50%</a>
+    <a href="index.php?action=all_75" class="btn btn-primary">75%</a>
+    <a href="index.php?action=all_100" class="btn btn-primary">100%</a>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
