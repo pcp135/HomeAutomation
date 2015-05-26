9883d19260cfbed33bcaa41bd313eb48b6a2a2b2
Phil Parsons <phil@parsons.uk.com>
2015-05-27 00:16:56 +0200

    Modified java for bootstrap 3

--- index.php
+++ index.php
@@ -226,7 +226,7 @@
     <script>
       $(function() { 
       //for bootstrap 3 use 'shown.bs.tab' instead of 'shown' in the next line
-      $('a[data-toggle="tab"]').on('shown', function (e) {
+      $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
       //save the latest tab; use cookies if you like 'em better:
       localStorage.setItem('lastTab', $(e.target).attr('id'));
       });
