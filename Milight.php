936e8d44dbe545c82550c31f558ac811301ce0df
Phil Parsons <phil@parsons.uk.com>
2015-05-31 00:20:36 +0200

    Added () around ternary statements

--- Milight.php
+++ Milight.php
@@ -441,9 +441,9 @@ class Milight {
       $h = $s = 0;
     }
     else {
-      $s = $l > 0.5 ? $d / (2 - $max - $min) : $d / ($max + $min);
+      $s = ($l > 0.5) ? $d / (2 - $max - $min) : $d / ($max + $min);
       switch ($max) {
-	case $r: $h = ($g - $b) / $d + ($g < $b ? 6 : 0); break;
+	case $r: $h = ($g - $b) / $d + (($g < $b) ? 6 : 0); break;
         case $g: $h = ($b - $r) / $d + 2; break;
         case $b: $h = ($r - $g) / $d + 4; break;
       }
